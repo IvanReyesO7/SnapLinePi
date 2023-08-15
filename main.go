04e83b282e0335ec39cb12a5d4f03996fe9f75f6
Ivan <ivanwilf@hotmail.com>
2023-08-15 13:37:06 +0100

    Added new godotenv package

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"server-test/controllers"
 	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load(".env")
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
@@ -21,7 +25,10 @@ func main() {
 		MaxAge:           12 * time.Hour,
 	}))
 
-	lineController := controllers.NewLineController()
+	lineController, err := controllers.NewLineController()
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Could not load Line controller, err: %e", err))
+	}
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"Status": "Server is up and running!"})
