5189b394b326937fbf39e6c650affc9ee109eb17
Ivan <ivanwilf@hotmail.com>
2023-08-29 15:59:36 +0000

    Completed cleaner task

--- tasks/cleaner.go
+++ tasks/cleaner.go
@@ -1,26 +1,44 @@
 package tasks
 
 import (
-	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
-func CleanTempFiles() {
-	log.Println("Starting cleaner task...")
-	tmpPath := os.Getenv("PWD") + "/tmp" // specify the directory
+func CleanTempDir() {
+	time.Sleep(time.Second)
+	RemoveTempFiles()
+	log.Println("[TASK] Sleeping for 30 minutes")
+	ticker := time.NewTicker(30 * time.Minute)
+	for range ticker.C {
+		RemoveTempFiles()
+		log.Println("[TASK] Sleeping for 30 minutes")
+	}
+}
+
+func RemoveTempFiles() {
+	log.Println("[TASK] Starting cleaner task...")
+	tmpPath := os.Getenv("PWD") + "/tmp"
 
 	files, err := os.ReadDir(tmpPath)
 	if err != nil {
-		fmt.Println("Error reading directory:", err)
+		log.Println("[TASK] Error reading directory:", err)
 		return
 	}
 
+	if len(files) == 0 {
+		log.Println("[TASK] tmp directory is empty")
+		return
+	}
 	for _, file := range files {
 		if file.IsDir() {
 			continue
 		} else {
-			os.Remove(tmpPath + "/" + file.Name())
+			fileName := file.Name()
+			os.Remove(tmpPath + "/" + fileName)
+			log.Printf("[TASK] %s file removed succesfully", fileName)
 		}
 	}
+	return
 }
