49bad63796605f99e189b19cafba7521854c2116
Ivan <ivanwilf@hotmail.com>
2023-11-25 08:59:37 +0000

    Cahnged bubble to flex

--- services/line.go
+++ services/line.go
@@ -64,7 +64,7 @@ func BuildFlexSnapshotMessage(imageUrl string, text string) *linebot.FlexMessage
 	return flexMessage
 }
 
-func BuildFlexClipMessage(videoUrl string, previewImageUrl string) *linebot.BubbleContainer {
+func BuildFlexClipMessage(videoUrl string, previewImageUrl string, text string) *linebot.FlexMessage {
 	hero := linebot.VideoComponent{
 		Type:       linebot.FlexComponentTypeVideo,
 		URL:        videoUrl,
@@ -79,11 +79,58 @@ func BuildFlexClipMessage(videoUrl string, previewImageUrl string) *linebot.Bubb
 		AspectRatio: "20:13",
 	}
 
-	bubble := linebot.BubbleContainer{
+	flexContainer := &linebot.BubbleContainer{
 		Type: linebot.FlexContainerTypeBubble,
-		Size: linebot.FlexBubbleSizeTypeMega,
 		Hero: &hero,
+		Body: &linebot.BoxComponent{
+			Type:   linebot.FlexComponentTypeBox,
+			Layout: linebot.FlexBoxLayoutTypeVertical,
+			Contents: []linebot.FlexComponent{
+				&linebot.TextComponent{
+					Type:   linebot.FlexComponentTypeText,
+					Text:   "Clip",
+					Weight: linebot.FlexTextWeightTypeBold,
+					Size:   linebot.FlexTextSizeTypeXl,
+				},
+				&linebot.BoxComponent{
+					Type:    linebot.FlexComponentTypeBox,
+					Layout:  linebot.FlexBoxLayoutTypeVertical,
+					Margin:  linebot.FlexComponentMarginTypeLg,
+					Spacing: linebot.FlexComponentSpacingTypeSm,
+					Contents: []linebot.FlexComponent{
+						&linebot.TextComponent{
+							Type: linebot.FlexComponentTypeText,
+							Text: "Clip taken successfully",
+						},
+						&linebot.BoxComponent{
+							Type:    linebot.FlexComponentTypeBox,
+							Layout:  linebot.FlexBoxLayoutTypeBaseline,
+							Spacing: linebot.FlexComponentSpacingTypeSm,
+							Contents: []linebot.FlexComponent{
+								&linebot.TextComponent{
+									Type:  linebot.FlexComponentTypeText,
+									Text:  "Time",
+									Color: "#aaaaaa",
+									Size:  linebot.FlexTextSizeTypeSm,
+									Flex:  linebot.IntPtr(1),
+								},
+								&linebot.TextComponent{
+									Type:  linebot.FlexComponentTypeText,
+									Text:  text,
+									Wrap:  true,
+									Color: "#666666",
+									Size:  linebot.FlexTextSizeTypeSm,
+									Flex:  linebot.IntPtr(5),
+									Style: linebot.FlexTextStyleTypeItalic,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 
-	return &bubble
+	flexMessage := linebot.NewFlexMessage("Snapshot Message", flexContainer)
+	return flexMessage
 }
